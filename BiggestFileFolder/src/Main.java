8966773f28c20e1088ef2e37949752656fdcb400
Agrab <agrabov@mail.ru>
2020-07-25 14:33:57 +0300

    add humanReadeble

--- BiggestFileFolder/src/Main.java
+++ BiggestFileFolder/src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,7 +20,12 @@ public class Main {
 
         System.out.println("FileVisitor: " + size);
 
-
+        System.out.println("-----------");
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size2 = pool.invoke(calculator);
+        System.out.println(humanReadable(size2, SI.YES));
     }
 
     public static Long getFolderSize(File file) {
@@ -48,4 +54,29 @@ public class Main {
         Files.walkFileTree(path, fileVisitor);
         return fileVisitor.getFilesCount();
     }
+    enum SI {
+        YES(1024),
+        NO(1000);
+
+        int value;
+
+        SI(int value) {
+            this.value = value;
+        }
+    }
+
+    private static String humanReadable(long bytes, SI si)
+    {
+        int unit = si.value;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre;
+        if (si.value == 1000){
+            pre = "kMGTPE".charAt(exp - 1) + "";}
+        else{ pre = "kMGTPE".charAt(exp - 1) + "i";}
+        if (si.value == 1024) pre = "KMGTPE".charAt(exp - 1) + "";
+        else pre = "KMGTPE".charAt(exp - 1) + "i";
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+
 }
